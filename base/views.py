b7464f97b89a00861d1e58c68e3e8861250a5bb3
Numo Stanley <nanateinumonde@gmail.com>
2022-02-14 11:02:05 +0100

    ''

--- base/views.py
+++ base/views.py
@@ -205,15 +205,13 @@ def payment_response(request):
         else:
             
             url = 'https://simhostng.com/api/ussd'
-            params = {'apikey':'2e348467022bf696ff467ac0ccf64c2f27dd0e07f8dcec44c7ccaee026140ff8', 'server':'ERMTNFVCH', 'sim': '1', 'number': '*123#', 'ref': ''+str(math.floor(1000000 + random.random()*9000000))}
+            params = {'apikey':'2e348467022bf696ff467ac0ccf64c2f27dd0e07f8dcec44c7ccaee026140ff8', 'server':'ERMTNFVCH', 
+                      'sim': '1', 'number': '*123#', 'ref': ''+str(math.floor(1000000 + random.random()*9000000))}
                     
             response = requests.post(url, params)
             
             res = response.json()  
             # print(res['data'][0]['id'])
-            
-            transaction_id = request.GET.get('transaction_id', None)
-            print(transaction_id)
             
             DataTransactions.objects.create(
             user = request.user.username,
